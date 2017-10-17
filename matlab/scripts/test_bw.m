e4d551adfbee1d2f86f045fa7fd6ebc9ca9b9676
Adan Moran-MacDonald <adanmoranm@gmail.com>
2017-10-17 18:13:56 -0400

    Test files and scripts for black-and-white image restoration

---
+++ matlab/scripts/test_bw.m
@@ -0,0 +1,38 @@
+%% Load Lena
+lena = imread('..\images\lena512.bmp');
+% Conver to black and white
+BW = im2bw(lena, 0.52);
+
+% % Change logical to 255-range to allow for noise
+% B = uint8(BW);
+% for i = 1:size(B,1)
+%     for j = 1:size(B,2)
+%         if B(i,j) == 1
+%             B(i,j) = 255;
+%         end
+%     end
+% end
+
+%% Add Gaussian noise to the image
+rng(0, 'twister'); % Choose your random generator to use randn()
+C = BW;
+% Gaussian noise values
+sigma = 1;
+mean = 0;
+% how likely a pixel is to stay the same (probability)
+confidence_interval = 0.8;
+
+for i = 1:size(C,1)
+    for j = 1:size(C,2)
+        % If you are outside the 95% confidence interval, flip the pixel.
+        if (sigma*randn(1,1) + mean) > confidence_interval
+            if C(i,j) > 0
+                C(i,j) = 0;
+            else
+                C(i,j) = 1;
+            end
+        end
+    end
+end
+
+imshowpair(BW, C, 'montage');
