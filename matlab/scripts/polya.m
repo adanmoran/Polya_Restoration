76d15c08b9f189bc367ebdb704d79bf7e021bb41
Adan Moran-MacDonald <adanmoranm@gmail.com>
2017-10-20 11:54:41 -0400

    Added Polya Function
    This function takes in a graph (Nodes and Vertices) with their
    associated urns, and computes the next superurn iteration.

---
+++ matlab/scripts/polya.m
@@ -0,0 +1,39 @@
+%% Polya Contagion Model
+% This model 
+% V = vertices; A nxk matrix where n is the number of vertices, and k is
+% the number of balls
+%
+% E = superurn edge map; a binary nxn matrix where a 1 represents 
+% a connection between the vertices (i,j). Note that E must contain the
+% 1's along the diagonal so that the superurn can be correctly retrieved
+% from it.
+%
+% Delta = a kxk matrix where the (i,j)th entry denotes the number of balls 
+% of type j that will be added when a ball of type i is drawn.
+function newV = polya(V, E, Delta)
+    % How many types of balls we have
+    NUM_TYPES = size(Delta,1);
+    
+    % Compute the superurn of each pixel. As a matrix, this is E*V
+    superurn = E*V;
+    
+    % Pull a ball from the superurn and create a matrix, the same size as
+    % V, which has row i equal to the row of Delta for the ball we pulled.
+    % For example, if we pull a ball of type j, then the i'th row of this
+    % new matrix is row j of Delta.
+    balls_to_add = zeros(size(V));
+    
+    for i = 1:size(superurn,1)
+        % Compute total number of balls in this vertex
+        total_balls = sum(superurn(i,:));
+        % Decide the probability that you will draw each type of ball.
+        weightings = superurn(i,:) / total_balls;
+        % Randomly draw a ball from the superurn
+        ball_type_drawn = randsample(NUM_TYPES, 1, true, weightings);
+        % Add the number of balls according to the type we pulled
+        balls_to_add(i,:) = Delta(ball_type_drawn,:);
+    end
+    
+    % Add the balls to the original graph
+    newV = V + balls_to_add;
+end
