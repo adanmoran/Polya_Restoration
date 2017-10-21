987d8dc0bfb2642d43513deeb145728dbc7c8dd3
Adan Moran-MacDonald <adanmoranm@gmail.com>
2017-10-21 12:54:39 -0400

    Added a test script for plotting the events of the Polya urn. Also added a gitignore for internal matlab files.

---
+++ matlab/scripts/test_graph_plot.m
@@ -0,0 +1,32 @@
+% Create a Polya Model graph
+V = [5 5; 
+     6 4; 
+     7 3; 
+     4 6; 
+     3 7];
+E = [ 0 1 0 0 0;
+      0 0 1 0 0;
+      0 0 0 1 1;
+      0 0 0 0 1;
+      0 0 0 0 0];
+E = E + E' + eye(5);
+Delta = eye(size(V,2))*2;
+
+% Create a graph which we can see with plotting
+G = graph(E, 'OmitSelfLoops');
+
+% Plot the graph and make the nodes more visible
+p = plot(G, 'MarkerSize', 10);
+
+% Test the Polya Scheme on the graph
+for n = 1:10
+    V = polya(V, E, Delta);
+    % Colour each node and label it with its urn contents
+    for i = 1:length(V)
+        s = sum(V(i,:));
+        highlight(p, i, 'NodeColor', [ V(i,1)/s 0 V(i,2)/s]);
+        p.NodeLabel{i} = strcat('(',num2str(V(i,1)),'r ,', num2str(V(i,2)), 'b)');
+    end
+    % Pause for visual effect
+    pause(0.5);
+end
