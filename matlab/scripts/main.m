e93d04454481bcf67f9f78fd48f7c1aeafd72d3e
Christopher Ko <chriskopher@gmail.com>
2017-11-03 23:03:02 -0400

    moved variable parameters to top of script

--- matlab/scripts/main.m
+++ matlab/scripts/main.m
@@ -1,35 +1,58 @@
 %% Reset workspace
 clear; close all; clc;
 
+%% Declare Variables/Parameters
+% Image Things
+image_location = '..\images\lena512.bmp';
+image_binarize_thresh = 0.52;
+
+% Gaussian Noise Parameters
+gaussian_std_dev = 1;
+gaussian_mean = 0;
+gaussian_confidence_interval = 0.8;
+
+% Edge Detection Parameters
+edge_type = 'canny';
+edge_thresh = 0.8;
+edge_sigma = 0.9;
+
+% Adjacency Matrix Parameters
+adj_radius = 1;
+adj_norm = 1;                   % can be 1, 2, or 'inf'
+
+% Polya Parameters
+polya_iterations = 2;
+
 %% Load Binary Image
-lena = imread('..\images\lena512.bmp');
-lena_bw = imbinarize(lena, 0.52); % binarize image
+image = imread(image_location);
+image_bw = imbinarize(image, image_binarize_thresh); % binarize image
 
 %% Precomputation before Polya Model
 % Add Gaussian noise to binarized image
-lena_bw_noise = add_gaussian_noise(lena_bw, 1, 0, 0.8);
+lena_bw_noise = add_gaussian_noise(image_bw,            ... 
+                                   gaussian_std_dev,    ... 
+                                   gaussian_mean,       ...
+                                   gaussian_confidence_interval);
 figure;
-imshowpair(lena_bw, lena_bw_noise, 'montage');
+imshowpair(image_bw, lena_bw_noise, 'montage');
 
 % Find edges in noisy image
-edges = edge(lena_bw_noise, 'canny', 0.8, 0.9);
+edges = edge(lena_bw_noise, edge_type, edge_thresh, edge_sigma);
 figure;
 imshowpair(lena_bw_noise, edges, 'montage');
 
 % Get image initial adjacency matrix with R = 1, Norm = 1
-adjacency = get_sparse_adj(size(lena_bw), 1, 1);
+adjacency = get_sparse_adj(size(image_bw), adj_radius, adj_norm);
 
 % Remove adjacency connections based on edge graph
 %adjacency = remove_edge_connections(adjacency, edges);
 
 %% Setup Polya Model
-num_iterations = 2;
-
 % Initialize urns
 % urns = initialize_poly_urns(adjacency);
-urns = ones(length(lena_bw) ^ 2, 2);
+urns = ones(length(image_bw) ^ 2, 2);
 Delta = eye(size(urns,2)) * 2;
 
-for n = 1:num_iterations+1
+for n = 1:polya_iterations+1
     urns = polya(urns, adjacency, Delta);
 end
