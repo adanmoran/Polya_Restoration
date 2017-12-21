ab6bd542f0f4ccefcb633a47d8151b70200b38b9
Adan Moran-MacDonald <adanmoranm@gmail.com>
2017-12-20 22:12:19 -0500

    Renamed noisy in test_grayscale to noisy_lena

--- matlab/scripts/test_grayscale.m
+++ matlab/scripts/test_grayscale.m
@@ -10,17 +10,17 @@ rng(0, 'twister');
 % Gaussian noise values
 sigma = 0.02;
 mean = 0;
-noisy = imnoise(lena, 'gaussian', mean, sigma);
-figure, imshowpair(lena, noisy, 'montage');
+noisy_lena = imnoise(lena, 'gaussian', mean, sigma);
+figure, imshowpair(lena, noisy_lena, 'montage');
 
 %% Take the Edge Map
 % The variance of Canny's gaussian filter. Default is sqrt(2)
 sigma = 3;
 % Threshold for the Canny method
 thresh = 0.2;
-edges = edge(noisy, 'canny', thresh, sigma);
+edges = edge(noisy_lena, 'canny', thresh, sigma);
 figure;
-imshowpair(noisy, edges, 'montage');
+imshowpair(noisy_lena, edges, 'montage');
 
 %% Build Adjacency Matrix
 adj_radius = 1;
@@ -28,8 +28,21 @@ adj_norm = 1;
 
 % Get image initial adjacency matrix
 adjacency = get_sparse_adj(size(lena), adj_radius, adj_norm);
+% Remove adjacency connections based on edge map
+adjacency = adjacency_minus_edge_d(adjacency, edges, adj_radius);
 
 %% Setup Polya Model
 starting_balls = 10;
 % Initialize urns with standard adjacency matrix
-urns = initialize_polya_urns(noisy, adjacency, starting_balls);
+urns = initialize_polya_urns(noisy_lena, adjacency, starting_balls);
+
+%% Iterate the polya model
+N = 10;
+
+for i = 1:N
+    %TODO: create the polya model for grayscale
+    % urns = polya(urns, adjacency, Delta, sample_type);
+end
+
+%% Build the final image
+output = image_from_urns(size(noisy_lena), urns);
