dded178757f8810a96099741882001252005be87
Christopher Ko <chriskopher@gmail.com>
2018-01-25 12:52:16 -0500

    renamed string parameters

--- matlab/scripts/inverse_quantize_image.m
+++ matlab/scripts/inverse_quantize_image.m
@@ -4,7 +4,7 @@ function image = inverse_quantize_image(q_image, num_ball_types, inverse_quantiz
     % Get the max size of that type, plus one for zero
     numberOfColours = double(intmax(classname)) + 1;
 
-    if strcmp(inverse_quantization, 'mid') || strcmp(inverse_quantization, 'middle')
+    if strcmp(inverse_quantization, 'low') || strcmp(inverse_quantization, 'lower')
         image = floor(q_image .* (numberOfColours/(num_ball_types-1)));
     end
 end
