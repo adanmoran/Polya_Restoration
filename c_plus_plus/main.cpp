e2ee7c5935aa1de8f2c1bdc48dd89808988d3261
Adan Moran-MacDonald <adanmoranm@gmail.com>
2018-01-25 11:39:18 -0500

    Add sparse matrix to main

--- c_plus_plus/main.cpp
+++ c_plus_plus/main.cpp
@@ -2,10 +2,15 @@
 // Copyright Adan Moran-MacDonald, 2018 //
 //////////////////////////////////////////
 
-#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
+typedef Eigen::SparseMatrix<double> SparseMatrix; // declares a column-major sparse matrix type of double
 int main()
 {
+    // build a nxm sparse matrix
+    int n = 5;
+    int m = 5;
+    SparseMatrix A(n, m);
 	return 0;
 }
 
