9c0b1fe826de26a1c08e7a46fde0714bb8babe0a
Adan Moran-MacDonald <adanmoranm@gmail.com>
2018-01-25 11:55:12 -0500

    Create a sparse matrix example in main.cpp, where we build an identity 5x5 and then print it out by converting it to a full matrix

--- c_plus_plus/main.cpp
+++ c_plus_plus/main.cpp
@@ -3,14 +3,32 @@
 //////////////////////////////////////////
 
 #include <Eigen/Sparse>
+#include <Eigen/Dense>
+#include <vector>
+#include <iostream>
 
-typedef Eigen::SparseMatrix<double> SparseMatrix; // declares a column-major sparse matrix type of double
+typedef Eigen::SparseMatrix<int> SparseMatrix; // declares a column-major sparse matrix type of double
+typedef Eigen::Triplet<int> Triplet; // declarse a triplet type for doubles
+typedef std::vector<Triplet> Triplets;
 int main()
 {
     // build a nxm sparse matrix
     int n = 5;
     int m = 5;
     SparseMatrix A(n, m);
+
+    // build a set of triplets
+    Triplets elements;
+    elements.reserve(n);
+    for (int i = 0; i < n; ++i)
+    {
+        elements.push_back(Triplet(i,i,1));
+    }
+
+    A.setFromTriplets(elements.begin(), elements.end());
+
+    std::cout << Eigen::MatrixXi(A) << std::endl;
+
 	return 0;
 }
 
