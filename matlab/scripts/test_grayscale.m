4a80072c972d5527fcb9a066c8c51e984ed01979
Adan Moran-MacDonald <adanmoranm@gmail.com>
2018-02-01 12:22:50 -0500

    Add burst noise for grayscale images, and compare burst noise with polya vs median restoration. Turns out Polya is better for burst and gaussian noise!

--- matlab/scripts/test_grayscale.m
+++ matlab/scripts/test_grayscale.m
@@ -1,10 +1,9 @@
 clear all
 close all
 clc
-%% Load Lena in Grayscale
-lena = rgb2gray(imread('..\images\oil_spill.jpg'));
-lena(343:500, :) = lena(343);
-%lena = imread('..\images\lena512.bmp');
+%% Load Image in Grayscale
+image = imread('..\images\lena512.bmp');
+% image = rgb2gray(imread('..\images\oil_spill.jpg'));
 
 %% Add Gaussian Noise
 rng(0, 'twister');
@@ -12,27 +11,55 @@ rng(0, 'twister');
 % Gaussian noise values
 sigma = 0.01;
 mean = 0;
-noisy_lena = lena; %imnoise(lena, 'gaussian', mean, sigma);
-figure, imshowpair(lena, noisy_lena, 'montage');
+% Bursty noise values
+burstType = 'binary'; % 'gaussian' or 'binary'
+correlation = 0.98;
+error = 0.1; % 0.2 for gaussian, 0.1 for binary
+burstMean = 0;
+burstSigma = 100;
+% Noise type
+% 'gaussian' or 'burst' or 'both'
+noise = 'both';
+
+noisy_image = image;
+
+if strcmp(noise, 'gaussian')
+    noisy_image = imnoise(image, 'gaussian', mean, sigma);
+elseif strcmp(noise, 'burst')
+    if strcmp(burstType,'binary')
+        noisy_image = add_bursty_noise(image, correlation, error, 'binary');
+    else
+        noisy_image = add_bursty_noise(image, correlation, error, 'gaussian', burstMean, burstSigma);
+    end
+elseif strcmp(noise, 'both')
+    noisy_image = imnoise(image, 'gaussian', mean, sigma);
+    if strcmp(burstType,'binary')
+        noisy_image = add_bursty_noise(noisy_image, correlation, error, 'binary');
+    else
+        noisy_image = add_bursty_noise(noisy_image, correlation, error, 'gaussian', burstMean, burstSigma);
+    end
+end
+
+figure, imshowpair(image, noisy_image, 'montage');
 
 %% Take the Edge Map
 % The variance of Canny's gaussian filter. Default is sqrt(2)
 sigma = 3;
 % Threshold for the Canny method
 % Oil Spill: 0.4
-% Lena: 0.2
+% Lena: with gauss = 0.2, with burst = 0.38
 % Two ships: 0.5
-thresh = 0.4;
-edges = edge(noisy_lena, 'canny', thresh, sigma);
+thresh = 0.38;
+edges = edge(noisy_image, 'canny', thresh, sigma);
 figure;
-imshowpair(noisy_lena, edges, 'montage');
+imshowpair(noisy_image, edges, 'montage');
 
 %% Build Adjacency Matrix
-adj_radius = 2;
+adj_radius = 3;
 adj_norm = 1;
 
 % Get image initial adjacency matrix
-adjacency = get_sparse_adj(size(lena), adj_radius, adj_norm);
+adjacency = get_sparse_adj(size(image), adj_radius, adj_norm);
 
 %% Setup Polya Model
 % Sample Type (random, median)
@@ -44,13 +71,13 @@ balls_to_add = 75;
 % Matrix of ball addition
 Delta = balls_to_add * eye(256);
 % Initialize urns with standard adjacency matrix
-urns = initialize_polya_urns(noisy_lena, adjacency, starting_balls);
+urns = initialize_polya_urns(noisy_image, adjacency, starting_balls);
 
 % Initialize the image for the median filter comparison
-medianed = noisy_lena;
+medianed = noisy_image;
 
 %% Iterate the polya model
-N = 6;
+N = 8;
 
 for i = 1:N
     tic
@@ -63,18 +90,18 @@ end
 
 %% Build the final image
 tic
-output = uint8(image_from_urns(size(noisy_lena), urns));
+output = uint8(image_from_urns(size(noisy_image), urns));
 toc
 figure;
-imshowpair(noisy_lena, output, 'montage');
+imshowpair(noisy_image, output, 'montage');
 title ('Noise vs Polya');
-fprintf('Polya vs Original MSE: %.3f\n', immse(output, lena));
-fprintf(' ---- ssim: %.3f\n', ssim(output, lena));
+fprintf('Polya vs Original MSE: %.3f\n', immse(output, image));
+fprintf(' ---- ssim: %.3f\n', ssim(output, image));
 figure;
-imshowpair(noisy_lena, medianed, 'montage');
+imshowpair(noisy_image, medianed, 'montage');
 title('Noise vs Median');
-fprintf('Median vs Original MSE: %.3f\n', immse(medianed, lena));
-fprintf(' ---- ssim: %.3f\n', ssim(medianed, lena));
+fprintf('Median vs Original MSE: %.3f\n', immse(medianed, image));
+fprintf(' ---- ssim: %.3f\n', ssim(medianed, image));
 figure;
 imshowpair(output, medianed, 'montage');
 title('Polya vs Median');
